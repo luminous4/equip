38b4618164bd9ac7b6071225028f02efbc2ef671
Joe Prempeh <prempehjoe@gmail.com>
2015-06-17 11:51:42 -0700

    feat(server): server serves client folder

--- server/server.js
+++ server/server.js
@@ -3,8 +3,9 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '../client'));
+app.use(express.static(__dirname + '/../client'));
 
 var server = app.listen(port, function () {
 	console.log('Example app listening at http://localhost:%s', port);
 });
+
