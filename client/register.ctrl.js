ca7bab0709ebfccfe38ecd8e5080b49f292708e8
Christiane Kammerl <christiane@kammerl.com>
2015-06-18 15:11:20 -0700

    dev(signup) saves new user to firebase

--- client/register.ctrl.js
+++ client/register.ctrl.js
@@ -1,13 +1,11 @@
 angular.module('equip')
 
   .controller('registerCtrl', function($scope, $firebaseAuth, $location, User, refUrl) {
-    var email = this.email;
-    var password = this.password;
     var ref = new Firebase(refUrl);
     var authObj = $firebaseAuth(ref);
 
     this.register = function() {
       console.log('just clicked register button');
-      // User.register(email, password, authObj);
+      User.register(this.email, this.password, authObj);
     };
   })
