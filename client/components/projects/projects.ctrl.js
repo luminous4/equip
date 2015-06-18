458ffea9358f36038eef73721a9dfe4afbff403b
kenning <hwerzog@gmail.com>
2015-06-18 13:14:17 -0700

    dev(project): add empty project create and project view files

--- client/components/projects/projects.ctrl.js
+++ client/components/projects/projects.ctrl.js
@@ -7,108 +7,7 @@
   // may need a form for creating a new project
     // createproject.ctrl.js
 
-(function() {
-  var app = angular.module('shoppingList', []);
-
-  app.controller('ProjectController', function() {
-    this.catToAdd = "";
-    this.addCatToAdd = function() {
-      if(this.catToAdd == "") return;
-      this.supertypeToAdd.cats.push(this.catToAdd);
-      this.catToAdd = "";
-    };
-    this.removeCatToAdd = function(catToRemove) {
-      for(var i = 0; i < this.supertypeToAdd.cats.length; i++){
-        if(this.supertypeToAdd.cats[i] == catToRemove && catToRemove !== "All") {
-          this.supertypeToAdd.cats.splice(i,1);
-          break;
-        }
-      }
-    };
-    this.removeCat = function(catToRemove) {
-      for(var i = 0; i < this.cats[this.catToEdit].length; i++){
-        if(this.cats[this.catToEdit][i] === catToRemove) {
-          this.cats[this.catToEdit].splice(i, 1);
-        }
-      }
-    };
-    this.addCat = function(){
-      if(this.catToAdd == "") return;
-      this.cats[this.catToEdit].push(this.catToAdd);
-      this.catToAdd = "";
-    }
-
-    this.addSuperType = function() {
-      if(this.supertypeToAdd.cats.length < 1) return;
-
-      var newCatName = this.supertypeToAdd.name;
-      this.catSupertypes.push(newCatName);
-
-      this.alertMessage = "Created new category " + newCatName;
-
-      this.cats[this.supertypeToAdd.name] = this.supertypeToAdd.cats;
-
-
-      this.supertypeToAdd = {
-      name: "",
-      cats: ['All']
-      };
-      this.selectedTab = "All";
-      this.alertMessage = "Created new category " + newCatName;
-    };
-    this.addSupercat = function() {
-      this.cats[this.catToEdit] = null;
-      for(var i = 0; i < this.catSupertypes.length; i++){
-        if(this.cats[this.catToEdit] === this.catSupertypes[i]) {
-          this.catSupertypes.splice(i, 1);
-        }
-      }
-    }
-
-    this.catToEdit = "";
-    this.editCategory = function(editCategory) {
-      this.selectedTab = "Edit Category";
-      this.catToEdit = editCategory;
-    };
-
-    this.cats = {
-      Stores: ["All", "Whole foods", "Ralphs"],
-      Aisles: ["All", "Bakery", "Freezer", "Fruits", "Utensils", "Vegetables" ]
-    };
-
-    this.selectedTab = "All";
-    this.selectedCategory = false;
-    this.selectTab = function(newTab) {
-      this.selectedTab = newTab;
-      this.alertMessage = "";
-    };
-    this.selectCategory = function(newTab, newCat) {
-      this.selectedTab = newTab;
-      this.selectedCategory = newCat;
-      this.alertMessage = "";
-    };
-  });
-
-  app.controller('ListController', function() {
-    this.products = products;
-
-    this.newProduct = { };
-
-    this.addProduct = function() {
-      this.products.push(this.newProduct);
-      this.newProduct = { };
-    };
-    this.removeProduct = function(product) {
-      for(var i = 0; i < this.products.length; i++) {
-        if(this.products[i] === product) {
-          this.products.splice(i, 1);
-          break;
-        }
-      }
-    };
-  });
-
-  var products = [
+  var projectArray = [
     {
       name: 'Rebuild the zoo',
       label: 'Zoo',
@@ -127,6 +26,51 @@
         }
       ],
       calendarEvents: []
-    } 
+    },
+    {
+      name: 'Eat garbage',
+      label: 'Zoo',
+      userList: [
+        {
+          name: "Pig",
+          icon: 'img/pig.png'
+        },
+      ],
+      calendarEvents: []
+    }  
   ];
+
+(function() {
+  angular.module('equip');
+
+  app.controller('ProjectListController', function() {
+    this.projects = projectArray;
+  });
+
+  app.filter('searchFor', function(){
+
+  return function(arr, searchString){
+
+    if(!searchString){
+      return arr;
+    }
+
+    var result = [];
+
+    searchString = searchString.toLowerCase();
+
+    // Using the forEach helper method to loop through the array
+    angular.forEach(arr, function(item){
+
+      if(item.title.toLowerCase().indexOf(searchString) !== -1){
+        result.push(item);
+      }
+
+    });
+
+    return result;
+  };
+
+})
+
 })();
