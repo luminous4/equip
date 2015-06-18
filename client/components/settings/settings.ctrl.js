204bf2ad3def52b7eb020b8238af6b4403b78f41
Emily Shenfield <emily.shenfield@gmail.com>
2015-06-18 12:01:07 -0700

    refactor(settings): changed comment and removed validations from settings form

--- client/components/settings/settings.ctrl.js
+++ client/components/settings/settings.ctrl.js
@@ -3,7 +3,7 @@ angular.module('equip.settings', [])
 .controller('SettingsCtrl', function($scope) {
 
   this.saveUserInfo = function() {
-    //create firebase reference with the following info
+    //add to firebase reference with the following info
     console.log(this.displayName);
     console.log(this.phoneNumber);
     console.log(this.imgUrl);
