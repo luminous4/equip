f6a6a2958ae32994712e6c791d88a60efaf5e848
Emily Shenfield <emily.shenfield@gmail.com>
2015-06-18 14:59:06 -0700

    dev(chat): added test functionality for chat with firebase

--- client/components/chat/chat.ctrl.js
+++ client/components/chat/chat.ctrl.js
@@ -1,5 +1,28 @@
 angular.module('equip')
 
-.controller('ChatCtrl', function($scope) {
+.controller('ChatCtrl', function($scope, $firebaseArray) {
+  var ref = new Firebase("https://mkseqip.firebaseIO.com");
+  var chatMessages = $firebaseArray(ref);
 
-});
+  this.poster = "Test";
+  this.messages = chatMessages;
+
+  this.addMessage = function() {
+    this.messages.$add({
+      user: this.poster,
+      message: this.post
+    });
+
+    this.message = "";
+  };
+
+  this.messages.$loaded(function() {
+    console.log(this.messages);
+    if (this.messages.length === 0) {
+      this.messages.$add({
+        user: "Emily",
+        message: "HEY!"
+      });
+    }
+  });
+})
