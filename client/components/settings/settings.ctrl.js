ddb3a6b30e5e80cd083043936935a7db32559116
Emily Shenfield <emily.shenfield@gmail.com>
2015-06-18 09:38:11 -0700

    dev(settings): added ng-models to input fields in user settings

--- client/components/settings/settings.ctrl.js
+++ client/components/settings/settings.ctrl.js
@@ -2,4 +2,11 @@ angular.module('equip.settings', [])
 
 .controller('SettingsCtrl', function($scope) {
 
+  this.saveUserInfo = function() {
+    //create firebase reference with the following info
+    console.log(this.displayName);
+    console.log(this.phoneNumber);
+    console.log(this.imgUrl);
+  };
+
 });
