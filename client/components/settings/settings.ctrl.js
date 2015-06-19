549e423551316d8abdc499ce244ba18e21e74f1f
Emily Shenfield <emily.shenfield@gmail.com>
2015-06-19 16:53:34 -0700

    dev(settings/chat): settings now sets data on user in firebase, chat can access userID

--- client/components/settings/settings.ctrl.js
+++ client/components/settings/settings.ctrl.js
@@ -1,12 +1,19 @@
 angular.module('equip')
 
-.controller('SettingsCtrl', function($scope) {
+.controller('SettingsCtrl', function($scope, $window, refUrl) {
+  
+  var userObj = $window.localStorage.getItem('firebase:session::mksequip');
+  var userId = JSON.parse(userObj).uid;
+
 
   this.saveUserInfo = function() {
     //add to firebase reference with the following info
-    console.log(this.displayName);
-    console.log(this.phoneNumber);
-    console.log(this.imgUrl);
+    var ref = new Firebase(refUrl);
+    ref.child("users").child(userId).update({
+      displayName: this.displayName,
+      phoneNumber: this.phoneNumber,
+      imgUrl: this.imgUrl
+    });
   };
 
 });
