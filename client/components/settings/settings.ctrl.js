f785de574701adc5c53705ca7a183500f266bf9f
Emily Shenfield <emily.shenfield@gmail.com>
2015-06-19 19:51:47 -0700

    dev(teams): added team list to user and user list to teams

--- client/components/settings/settings.ctrl.js
+++ client/components/settings/settings.ctrl.js
@@ -1,31 +1,52 @@
 angular.module('equip')
 
-.controller('SettingsCtrl', function($scope, $window, refUrl) {
+.controller('SettingsCtrl', function($scope, $window, $firebaseArray, refUrl) {
   
   var userObj = $window.localStorage.getItem('firebase:session::mksequip');
   var userId = JSON.parse(userObj).uid;
+  var ref = new Firebase(refUrl);
 
+  var checkIfTeamExists = function(teamName, cb) {
+    ref.child('teams').child(teamName).once('value', function(snapshot) {
+      var exists = (snapshot.val() !== null);
+      cb(teamName, exists);
+    });
+  }
 
   this.saveUserInfo = function() {
-    //add to firebase reference with the following info
-    var ref = new Firebase(refUrl);
     if (this.displayName) {
-      ref.child("users").child(userId).update({
-        displayName: this.displayName,
+      ref.child('users').child(userId).update({
+        displayName: this.displayName
       });
     }
+
+    if (this.teamName) {
+      checkIfTeamExists(this.teamName, function(teamName, exists) {
+        if(!exists) {
+          ref.child('teams').child(teamName).set({teamName: teamName, users: null});
+        }
+
+        var teamUsers = $firebaseArray(ref.child('teams').child(teamName).child('users'));
+        teamUsers.$add(userId);
+      })
+
+      var userTeams = $firebaseArray(ref.child('users').child(userId).child('teams'));
+      userTeams.$add(this.teamName);
+    }
+
     if (this.phoneNumber) {
-      ref.child("users").child(userId).update({
-        phoneNumber: this.phoneNumber,
+      ref.child('users').child(userId).update({
+        phoneNumber: this.phoneNumber
       });
     }
     if (this.imgUrl) {
-      ref.child("users").child(userId).update({
-        imgUrl: this.imgUrl,
+      ref.child('users').child(userId).update({
+        imgUrl: this.imgUrl
       });
     }
 
     this.displayName = "";
+    this.teamName = "";
     this.phoneNumber = "";
     this.imgUrl = "";
   };
