b01fbcbc0567a4e81eeccb9d125f437624b5c742
Christiane Kammerl <christiane@kammerl.com>
2015-06-22 18:28:06 -0700

    feat(calendar) updates adding events to db including reference to related user and project

--- client/components/calendar/calendar.ctrl.js
+++ client/components/calendar/calendar.ctrl.js
@@ -1,14 +1,12 @@
 angular.module('equip')
 
-  .controller('CalendarCtrl', function($scope, FirebaseFactory, User, refUrl) {
-    console.log('in CalendarCtrl');
-
+  .controller('CalendarCtrl', function($scope, FirebaseFactory) {
+    // console.log('in CalendarCtrl');
     var allUsers = FirebaseFactory.getCollection('users');
-    // console.log('all users', allUsers);
-    var currentUser = FirebaseFactory.getCurrentUser;
-    //currentUser.uid;
-    var allEvents = FirebaseFactory.getCollection('events');
-    // console.log('all events', allEvents);
+    var currentUser = FirebaseFactory.getCurrentUser();
+
+    //TODO (select from drop down)
+    var eventProjectId = this.project;
 
     this.saveNewEvent = function() {
       // console.log('add event clicked');
@@ -18,41 +16,11 @@ angular.module('equip')
       newEvent['end'] = this.endYear + ' ' + this.endMonth  + ' ' + this.endDay  + ' ' + this.endHour  + ':' + this.endMinutes;
       newEvent['allDay'] = false;
       newEvent['stick'] = true;
-      newEvent['users'] = [];
-      newEvent['projects'] = [];
+      newEvent['userId'] = currentUser.uid;
+      newEvent['projectId'] = eventProjectId || 'Test Project';
 
-      // console.log('newEvent', newEvent);
-      allEvents.$add(newEvent);
-      // console.log('eventSources after adding event', this.eventSources);
-      allEvents.child('users').$add({userId: userId});
-      // allEvents.child('projects').$add({projectId: projectId});
-    };
-
-    $scope.events = {
-      events: [
-        {
-          title: 'Event1',
-          start: '2015 06 20 14:30',
-          end: '2015 06 20 16:00',
-          id: '1',
-          url: '#/index/projects',
-          className: 'test',
-          allDay: false,
-          stick: true
-        },
-        {
-          title: 'Event2',
-          start: '2015 06 21 11:30',
-          end: '2015 06 21 12:00',
-          id: '2',
-          url: '#/index/projects',
-          className: 'anotherTest',
-          allDay: false,
-          stick: true
-        },
-      ],
-      color: 'lightblue',   // optional
-      textColor: 'black' // optional
+      FirebaseFactory.addToCollection('events', newEvent);
+      console.log('added event:', newEvent.title);
     };
 
     this.uiConfig = {
@@ -66,8 +34,8 @@ angular.module('equip')
         }
       }
   };
-    /* event sources array*/
 
-    this.eventSources = [allEvents];
-    console.log('eventSources', this.eventSources);
+  var allEvents = FirebaseFactory.getCollection('events');
+  this.eventSources = [allEvents];
+  // console.log('eventSources', this.eventSources);
 });
