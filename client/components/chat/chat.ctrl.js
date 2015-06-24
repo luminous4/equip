ca50d4c7dc178172d91e91c179ac4eba5a3ebcb4
Emily Shenfield <emily.shenfield@gmail.com>
2015-06-24 14:58:28 -0700

    dev(chat): added lastMessage time to keep track of interval between messages

--- client/components/chat/chat.ctrl.js
+++ client/components/chat/chat.ctrl.js
@@ -3,13 +3,15 @@ angular.module('equip')
 .controller('ChatCtrl', function($scope, $rootScope, $firebaseArray, $location, $window, User, refUrl, FirebaseFactory) {
 
   var userId = FirebaseFactory.getCurrentUser().uid;
-
   var ref = new Firebase(refUrl);
+
   this.canSend = true;
+  var lastMessageDate = 0;
 
   $rootScope.$watch('selectedTeam', function() {
     if ($rootScope.selectedTeam) {
       $scope.messages = FirebaseFactory.getCollection('messages');
+      lastMessageDate = 0;
     }
   });
 
@@ -25,17 +27,28 @@ angular.module('equip')
   });
 
   this.addMessage = function() {
+    var currentDate = new Date();
+    console.log("lastMessageDate: ", lastMessageDate);
+    console.log("current date: ", currentDate);
+    var showImg = true;
+    if (currentDate - lastMessageDate < 20000) {
+      console.log("been less than 20000 ms: ", currentDate - lastMessageDate);
+      showImg = false;
+    }
     var date = moment().format('YYYY-MM-DD hh:mm');
     if (!$rootScope.selectedTeam) {
       this.canSend = false;
     } else {
       $scope.messages.$add({
+        displayImg: showImg,
         chatName: $scope.user,
         userImg: $scope.img,
         text: this.message,
-        createdAt: date
+        createdAt: date,
       });      
     }
+    lastMessageDate = currentDate;
+    console.log("new lastMessageDate: ", lastMessageDate);
     this.message = '';
   };
 
