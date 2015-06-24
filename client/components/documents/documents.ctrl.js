c48a04765a01aa4b9c2300b026a0b341bcfda130
Joe Prempeh <prempehjoe@gmail.com>
2015-06-24 16:21:57 -0700

    fix(documents): documents watches  for selectedTeam

--- client/components/documents/documents.ctrl.js
+++ client/components/documents/documents.ctrl.js
@@ -3,17 +3,11 @@
 * */
 
 angular.module('equip')
-	.controller('DocumentCtrl', function($scope, FirebaseFactory){
-		var documentList = FirebaseFactory.getCollection('documents') || [];
-		$scope.documentList = documentList;
-
-		console.log(documentList)
-
-		/*Edit Product*/
-		$scope.selectedDoc = null;
-		$scope.editDoc = function(event) {
-			console.log(event);
-			//$state.go('index.texteditor', {title: });
-
-		}
+	.controller('DocumentCtrl', function($scope, $rootScope, FirebaseFactory){
+		$rootScope.$watch('selectedTeam', function(){
+			if($rootScope.selectedTeam) {
+				var documentList = FirebaseFactory.getCollection('documents');
+				$scope.documentList = documentList;
+			}
+		});
 	});
