8db075d57365dfce0798c605715179e687dde890
Emily Shenfield <emily.shenfield@gmail.com>
2015-06-25 15:03:38 -0700

    dev(settings): started to implement displaying user data in settings

--- client/components/settings/settings.ctrl.js
+++ client/components/settings/settings.ctrl.js
@@ -1,45 +1,56 @@
 angular.module('equip')
 
-.controller('SettingsCtrl', function($scope, $window, $firebaseArray, FirebaseFactory, refUrl) {
+.controller('SettingsCtrl', function($scope, FirebaseFactory) {
   
-  var userObj = $window.localStorage.getItem('firebase:session::mksequip');
-  var userId = JSON.parse(userObj).uid;
-  var ref = new Firebase(refUrl);
+  var userId = FirebaseFactory.getCurrentUser().uid;
 
-  var checkIfTeamExists = function(teamName, cb) {
-    ref.child('teams').child(teamName).once('value', function(snapshot) {
-      var exists = (snapshot.val() !== null);
-      cb(teamName, exists);
-    });
-  }
+  (this.getUserInfo = function() {
+    var userInfo = FirebaseFactory.getObject(['users', userId], true);
+    console.log('got user info!', userInfo);
+    $scope.showDisplayName = userInfo.displayName;
+    console.log("display name: ", $scope.showDisplayName);
+    $scope.showPhoneNumber = userInfo.phoneNumber;
+    $scope.showFacebook = userInfo.Facebook;
+    $scope.showGithub = userInfo.Github;
+    $scope.showLinkedIn = userInfo.LinkedIn;
+    $scope.showOther = userInfo.Other;
+  })();
 
   this.saveUserInfo = function() {
     if (this.displayName) {
       FirebaseFactory.updateItem(['users', userId], {displayName: this.displayName}, true);
     }
-
-    if (this.teamName) {
-      checkIfTeamExists(this.teamName, function(teamName, exists) {
-        if(!exists) {
-          FirebaseFactory.updateItem(['teams', teamName], {users: null}, true);
-        }
-        FirebaseFactory.addToCollection(['teams', teamName, 'users'], userId, true);
-      });
-
-      FirebaseFactory.addToCollection(['users', userId, 'teams'], this.teamName, true);
-    }
-
     if (this.phoneNumber) {
       FirebaseFactory.updateItem(['users', userId], {phoneNumber: this.phoneNumber}, true);
+    }
+    if (this.socialInput) {
+      newSocial = {};
+      newSocial[this.socialSelect] = this.socialInput;
+      FirebaseFactory.updateItem(['users', userId], newSocial, true)
     }
     if (this.imgUrl) {
       FirebaseFactory.updateItem(['users', userId], {imgUrl: this.imgUrl}, true);
     }
 
     this.displayName = '';
-    this.teamName = '';
+    this.socialInput = '';
+    this.socialSelect = null;
     this.phoneNumber = '';
     this.imgUrl = '';
   };
 
 });
+
+
+// Old team join: 
+
+// if (this.teamName) {
+//   checkIfTeamExists(this.teamName, function(teamName, exists) {
+//     if(!exists) {
+//       FirebaseFactory.updateItem(['teams', teamName], {users: null}, true);
+//     }
+//     FirebaseFactory.addToCollection(['teams', teamName, 'users'], userId, true);
+//   });
+
+//   FirebaseFactory.addToCollection(['users', userId, 'teams'], this.teamName, true);
+// }
