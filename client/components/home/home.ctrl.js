7ba0b758612a01b4ca6a442da2217de712536577
Joe Prempeh <prempehjoe@gmail.com>
2015-06-24 20:03:41 -0700

    feat(home): select team

--- client/components/home/home.ctrl.js
+++ client/components/home/home.ctrl.js
@@ -1,5 +1,24 @@
 angular.module('equip')
 
-  .controller('HomeCtrl', function($rootScope, $location, $window, $scope, User) {
+  .controller('HomeCtrl', function($rootScope, $location, $window, $scope, User, FirebaseFactory) {
 
-})
+    if($rootScope.selectedTeam) {
+      $scope.currentTeamOption = $rootScope.selectedTeam;
+    }
+
+    userId = FirebaseFactory.getCurrentUser().uid;
+
+    $scope.usersTeams = FirebaseFactory.getCollection(['users', userId, 'teams'], true);
+
+    $scope.usersTeams.$loaded()
+      .then(function() {
+        $scope.currentTeamOption = $rootScope.selectedTeam;
+      });
+
+    this.changeContext = function() {
+      console.log("changedContext: ", $scope.currentTeamOption);
+      $rootScope.selectedTeam = $scope.currentTeamOption;
+    };
+
+
+  })
