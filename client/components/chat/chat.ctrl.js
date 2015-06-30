049e64dcc512b5ec35ee6acb352c10ea04f2a916
Emily Shenfield <emily.shenfield@gmail.com>
2015-06-30 15:48:28 -0700

    refactor(chat): upated chat to calculate showPic on render

--- client/components/chat/chat.ctrl.js
+++ client/components/chat/chat.ctrl.js
@@ -4,10 +4,10 @@ angular.module('equip')
 .controller('ChatCtrl', function($scope, $rootScope, FirebaseFactory) {
 
   var userId = FirebaseFactory.getCurrentUser().uid;
-  var lastMessageDate = 0;
-  var lastMessage;
   var userData = FirebaseFactory.getObject(['users', userId], true);
   $scope.canSend = true;
+    $scope.lastAuthor = $scope.user;
+  $scope.showedPic = false;
 
   $rootScope.$watch('selectedTeam', function() {
     if ($rootScope.selectedTeam) {
@@ -16,12 +16,6 @@ angular.module('equip')
       .then(function(data) {
         $scope.dashboardMessages = data.slice(data.length - 5, data.length);
       })
-
-      $scope.messages.$watch(function(event) {
-        lastMessage = FirebaseFactory.getObject(['messages', event.key]);
-      });
-
-      lastMessageDate = 0;
     }
   });
 
@@ -31,26 +25,36 @@ angular.module('equip')
     $scope.img = userData.imgUrl;
   });
 
+  $scope.showImg = function(currMessageDate, currMessageName) {
+    var messageDate = new Date(currMessageDate);
+    var showPic = true;
+    var interval = messageDate - $scope.lastMessageDate;
+    if (interval < 20000 && $scope.lastAuthor === currMessageName) {
+      $scope.showedPic = false;
+      showPic = false;
+    } else {
+      $scope.showedPic = true;
+      showPic = true;      
+    }
+    $scope.lastAuthor = currMessageName;
+    $scope.lastMessageDate = new Date(currMessageDate);
+    return showPic;
+  }
+
+
   $scope.addMessage = function() {
     var currentDate = new Date();
-    var date = moment().format('YYYY-MM-DD hh:mm');
-    var showImg = true;
-
-    if (currentDate - lastMessageDate < 20000 && lastMessage.chatName === $scope.user) {
-      showImg = false;
-    }
+    currentDate = currentDate.toString();
     if (!$rootScope.selectedTeam) {
       this.canSend = false;
     } else {
       this.canSend = true;
       $scope.messages.$add({
-        displayImg: showImg,
         chatName: $scope.user,
         userImg: $scope.img,
         text: $scope.message,
-        createdAt: date,
+        createdAt: currentDate,
       });      
-      lastMessageDate = currentDate;
     }
 
     this.message = '';
