68297305a5dc79cf34bc7818794ae60ca7d915e2
kenning <hwerzog@gmail.com>
2015-06-30 13:46:52 -0700

    feat(todo): initialized todo list

---
+++ client/components/todo/todo.ctrl.js
@@ -0,0 +1,151 @@
+(function() {
+  angular.module('equip')
+
+  .controller('TodoController', function($scope, $state, $stateParams, FirebaseFactory,
+                                            $firebaseArray, $timeout,  $rootScope, refUrl, $firebaseObject) {
+
+    $scope.sortableOptions = {
+        connectWith: ".connectPanels",
+        handler: ".ibox-title"
+    };
+
+    $scope.todoList = [
+        {
+            content: 'Simply dummy text of the printing and typesetting industry.',
+            date: '12.10.2015',
+            statusClass: 'warning',
+            tagName: 'Mark'
+        },
+        {
+            content: 'Many desktop publishing packages and web page editors now use Lorem Ipsum as their default.',
+            date: '05.04.2015',
+            statusClass: 'success',
+            tagName: 'Tag'
+        },
+        {
+            content: 'Sometimes by accident, sometimes on purpose (injected humour and the like).',
+            date: '16.11.2015',
+            statusClass: 'info',
+            tagName: 'Mark'
+        },
+        {
+            content: 'All the Lorem Ipsum generators',
+            date: '06.10.2015',
+            statusClass: 'danger',
+            tagName: 'Tag'
+        },
+        {
+            content: 'Which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, '+
+            'injected humour, or non-characteristic words etc.',
+            date: '09.12.2015',
+            statusClass: 'warning',
+            tagName: 'Mark'
+        },
+        {
+            content: 'Packages and web page editors now use Lorem Ipsum as',
+            date: '08.04.2015',
+            statusClass: 'warning',
+            tagName: 'Mark'
+        },
+        {
+            content: 'Many desktop publishing packages and web page editors now use Lorem Ipsum as their default.',
+            date: '05.04.2015',
+            statusClass: 'success',
+            tagName: 'Tag'
+        },
+        {
+            content: 'Sometimes by accident, sometimes on purpose (injected humour and the like).',
+            date: '16.11.2015',
+            statusClass: 'info',
+            tagName: 'Tag'
+        }
+    ];
+    $scope.inProgressList = [
+        {
+            content: 'Quisque venenatis ante in porta suscipit.',
+            date: '12.10.2015',
+            statusClass: 'success',
+            tagName: 'Mark'
+        },
+        {
+            content: ' Phasellus sit amet tortor sed enim mollis accumsan in consequat orci.',
+            date: '05.04.2015',
+            statusClass: 'success',
+            tagName: 'Tag'
+        },
+        {
+            content: 'Ut porttitor augue non sapien mollis accumsan. Nulla non elit eget lacus elementum viverra.',
+            date: '09.12.2015',
+            statusClass: 'warning',
+            tagName: 'Tag'
+        },
+        {
+            content: 'Packages and web page editors now use Lorem Ipsum as',
+            date: '08.04.2015',
+            statusClass: 'info',
+            tagName: 'Tag'
+        },
+        {
+            content: 'Quisque lacinia tellus et odio ornare maximus.',
+            date: '05.04.2015',
+            statusClass: 'success',
+            tagName: 'Mark'
+        },
+        {
+            content: 'Enim mollis accumsan in consequat orci.',
+            date: '11.04.2015',
+            statusClass: 'danger',
+            tagName: 'Tag'
+        }
+    ];
+    $scope.completedList = [
+        {
+            content: 'Sometimes by accident, sometimes on purpose (injected humour and the like).',
+            date: '16.11.2015',
+            statusClass: 'info',
+            tagName: 'Mark'
+        },
+        {
+            content: 'Ut porttitor augue non sapien mollis accumsan. Nulla non elit eget lacus elementum viverra.',
+            date: '09.12.2015',
+            statusClass: 'warning',
+            tagName: 'Tag'
+        },
+        {
+            content: 'Packages and web page editors now use Lorem Ipsum as',
+            date: '08.04.2015',
+            statusClass: 'warning',
+            tagName: 'Tag'
+        },
+        {
+            content: 'Many desktop publishing packages and web page editors now use Lorem Ipsum as their default.',
+            date: '05.04.2015',
+            statusClass: 'success',
+            tagName: 'Mark'
+        },
+        {
+            content: 'Sometimes by accident, sometimes on purpose (injected humour and the like).',
+            date: '16.11.2015',
+            statusClass: 'info',
+            tagName: 'Tag'
+        },
+        {
+            content: 'Simply dummy text of the printing and typesetting industry.',
+            date: '12.10.2015',
+            statusClass: 'warning',
+            tagName: 'Mark'
+        },
+        {
+            content: 'Many desktop publishing packages and web page editors now use Lorem Ipsum as their default.',
+            date: '05.04.2015',
+            statusClass: 'success',
+            tagName: 'Mark'
+        }
+    ];
+
+    $scope.sortableOptions = {
+        connectWith: ".connectList"
+    };
+  });
+})();
+
