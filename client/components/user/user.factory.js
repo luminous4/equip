f1168a3fea92564ad8c859d46e8376d2609fffef
Christiane Kammerl <christiane@kammerl.com>
2015-06-29 22:21:58 -0700

    refactor(register) adds helper method .createNewUser()

--- client/components/user/user.factory.js
+++ client/components/user/user.factory.js
@@ -23,19 +23,17 @@
     };
 
     var register = function(email, password, teamName, teamAction, firebaseAuthObj, callback) {
-      console.log('teamName in user factory', teamName);
-      console.log('teamAction', teamAction);
-
       var allTeams = FirebaseFactory.getCollection('teams', true);
       var foundTeam = false;
       var targetTeam = {};
       var targetTeamId = '';
       var targetTeamUsers = [];
+
       var newUserId = '';
+      var newUserDetails = {};
 
       allTeams.$loaded()
         .then(function() {
-          console.log('allTeams', allTeams);
           for (var i = 0; i < allTeams.length; i++) {
             if (allTeams[i].$id === teamName) {
               targetTeam = allTeams[i];
@@ -46,49 +44,11 @@
           }
 
           if (foundTeam && (teamAction === 'join')) {
-            console.log('targetTeam found:', targetTeam);
             console.log('join a team');
 
-            // create new user
-            firebaseAuthObj.$createUser({
-              email: email,
-              password: password
-            })
-            .then(function(userData) {
-              $window.localStorage.setItem('equipAuth', userData.token);
-
-              newUserId = userData.uid;
-              var teamToJoin = {};
-              teamToJoin[targetTeamId] = targetTeamId;
-
-              var newUserDetails = {
-                email: email,
-                displayName: email.replace(/@.*/, ''),
-                imgUrl: 'http://www.gravatar.com/avatar/' + hashEmail(email) + '.jpg',
-                teams: teamToJoin
-              }
-
-              // save new user to db
-              ref.child('users').child(newUserId).set(newUserDetails);
-              // FirebaseFactory.addToCollection(['users', newUserId], newUserDetails, true);
-
-              // add new user to targetTeam
-              var tempUsers = [];
-              for (var i = 0; i < targetTeamUsers.length; i++) {
-                tempUsers.push(targetTeamUsers[i]);
-              }
-              tempUsers.push(newUserId);
-
-              FirebaseFactory.updateItem(['teams',targetTeamId], {users: tempUsers}, true);
-
-              $location.path('/login');
-              callback(true);
-            })
-            // error saving user
-            .catch(function(error) {
-              console.log('error:', + error);
-              callback(false);
-            });
+            // build and save new user
+            // false => does not add new team
+            createNewUser(email, password, targetTeamId, targetTeamUsers, false, firebaseAuthObj, callback);
           }
 
           //TODO: display info to user
@@ -101,41 +61,9 @@
 
             targetTeamId = teamName;
 
-            // create new user
-            firebaseAuthObj.$createUser({
-              email: email,
-              password: password
-            })
-            .then(function(userData) {
-              $window.localStorage.setItem('equipAuth', userData.token);
-
-              newUserId = userData.uid;
-
-              var teamToJoin = {};
-              teamToJoin[targetTeamId] = targetTeamId;
-
-              var newUserDetails = {
-                email: email,
-                displayName: email.replace(/@.*/, ''),
-                imgUrl: 'http://www.gravatar.com/avatar/' + hashEmail(email) + '.jpg',
-                teams: teamToJoin
-              };
-
-              // save new user to db
-              ref.child('users').child(newUserId).set(newUserDetails);
-              // FirebaseFactory.addToCollection(['users', newUserId], newUserDetails, true);
-
-              var now = moment().format('MMMM Do YYYY, h:mm a');
-              FirebaseFactory.updateItem(['teams', targetTeamId], {users: [newUserId], createdAt: now}, true);
-
-              $location.path('/login');
-              callback(true);
-            })
-            // error saving user
-            .catch(function(error) {
-              console.log('error:', + error);
-              callback(false);
-            });
+            // build and save new user
+            // true => adds new team
+            createNewUser(email, password, targetTeamId, targetTeamUsers, true, firebaseAuthObj, callback);
           }
 
           //TODO: display info to user
@@ -144,6 +72,54 @@
           }
         });
       };
+
+    var createNewUser = function(email, password, targetTeamId, targetTeamUsers, addsTargetTeam, firebaseAuthObj, callback) {
+      firebaseAuthObj.$createUser({
+        email: email,
+        password: password
+      })
+      .then(function(userData) {
+        $window.localStorage.setItem('equipAuth', userData.token);
+
+        newUserId = userData.uid;
+
+        var teamToJoin = {};
+        teamToJoin[targetTeamId] = targetTeamId;
+
+        var newUserDetails = {
+          email: email,
+          displayName: email.replace(/@.*/, ''),
+          imgUrl: 'http://www.gravatar.com/avatar/' + hashEmail(email) + '.jpg',
+          teams: teamToJoin
+        };
+
+        // save new user to db
+        ref.child('users').child(newUserId).set(newUserDetails);
+        // FirebaseFactory.addToCollection(['users', newUserId], newUserDetails, true);
+
+        // add new user to targetTeam's user list
+        var tempUsers = [];
+        for (var i = 0; i < targetTeamUsers.length; i++) {
+          tempUsers.push(targetTeamUsers[i]);
+        }
+        tempUsers.push(newUserId);
+
+        if (addsTargetTeam) {
+          var now = moment().format('MMMM Do YYYY, h:mm a');
+          FirebaseFactory.updateItem(['teams', targetTeamId], {users: tempUsers, createdAt: now}, true);
+        } else {
+          FirebaseFactory.updateItem(['teams',targetTeamId], {users: tempUsers}, true);
+        }
+
+        $location.path('/login');
+        callback(true);
+      })
+      // error saving user
+      .catch(function(error) {
+        console.log('error:', + error);
+        callback(false);
+      });
+    };
 
     var isAuth = function() {
       return !!$window.localStorage.getItem('equipAuth');
