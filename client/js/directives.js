80c7259e2311a7b353dd19c4dea18e8ffc0b0ce1
Christiane Kammerl <christiane@kammerl.com>
2015-07-02 20:00:21 -0700

    adds sidebar/nav directives and metisMenu plugin

--- client/js/directives.js
+++ client/js/directives.js
@@ -27,47 +27,49 @@ function iboxTools($timeout) {
         }
     };
 };
-// function minimalizaSidebar($timeout) {
-//     return {
-//         restrict: 'A',
-//         template: '<a class="navbar-minimalize minimalize-styl-2 btn btn-primary " href="" ng-click="minimalize()"><i class="fa fa-bars"></i></a>',
-//         controller: function ($scope, $element) {
-//             $scope.minimalize = function () {
-//                 $("body").toggleClass("mini-navbar");
-//                 if (!$('body').hasClass('mini-navbar') || $('body').hasClass('body-small')) {
-//                     // Hide menu in order to smoothly turn on when maximize menu
-//                     $('#side-menu').hide();
-//                     // For smoothly turn on menu
-//                     setTimeout(
-//                         function () {
-//                             $('#side-menu').fadeIn(500);
-//                         }, 100);
-//                 } else if ($('body').hasClass('fixed-sidebar')){
-//                     $('#side-menu').hide();
-//                     setTimeout(
-//                         function () {
-//                             $('#side-menu').fadeIn(500);
-//                         }, 300);
-//                 } else {
-//                     // Remove all inline style from jquery fadeIn function to reset menu state
-//                     $('#side-menu').removeAttr('style');
-//                 }
-//             }
-//         }
-//     };
-// };
-// function sideNavigation($timeout) {
-//     return {
-//         restrict: 'A',
-//         link: function(scope, element) {
-//             // Call the metsiMenu plugin and plug it to sidebar navigation
-//             $timeout(function(){
-//                 element.metisMenu();
 
-//             });
-//         }
-//     };
-// };
+function sideNavigation($timeout) {
+    return {
+        restrict: 'A',
+        link: function(scope, element) {
+            // Call the metisMenu plugin and plug it to sidebar navigation
+            $timeout(function(){
+                element.metisMenu();
+
+            });
+        }
+    };
+};
+
+function minimalizaSidebar($timeout) {
+    return {
+        restrict: 'A',
+        template: '<a class="navbar-minimalize minimalize-styl-2 btn btn-primary " href="" ng-click="minimalize()"><i class="fa fa-bars"></i></a>',
+        controller: function ($scope, $element) {
+            $scope.minimalize = function () {
+                $("body").toggleClass("mini-navbar");
+                if (!$('body').hasClass('mini-navbar') || $('body').hasClass('body-small')) {
+                    // Hide menu in order to smoothly turn on when maximize menu
+                    $('#side-menu').hide();
+                    // For smoothly turn on menu
+                    setTimeout(
+                        function () {
+                            $('#side-menu').fadeIn(500);
+                        }, 100);
+                } else if ($('body').hasClass('fixed-sidebar')){
+                    $('#side-menu').hide();
+                    setTimeout(
+                        function () {
+                            $('#side-menu').fadeIn(500);
+                        }, 300);
+                } else {
+                    // Remove all inline style from jquery fadeIn function to reset menu state
+                    $('#side-menu').removeAttr('style');
+                }
+            }
+        }
+    };
+};
 
 function fitHeight(){
     return {
@@ -81,8 +83,8 @@ function fitHeight(){
 
 angular
     .module('equip')
-    // .directive('sideNavigation', sideNavigation)
+    .directive('sideNavigation', sideNavigation)
     .directive('iboxTools', iboxTools)
     .directive('fitHeight', fitHeight)
-    // .directive('minimalizaSidebar', minimalizaSidebar)
- 
+    .directive('minimalizaSidebar', minimalizaSidebar)
+
