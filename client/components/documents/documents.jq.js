72c15a7c481b97bce14638f8b03ae29dff56f3a0
Joe Prempeh <prempehjoe@gmail.com>
2015-07-06 15:53:30 -0700

    feat(drive): image uploader uploads and shows images

--- client/components/documents/documents.jq.js
+++ client/components/documents/documents.jq.js
@@ -13,33 +13,23 @@ $(function() {
 				// This is your API key.
 				key: "INSERT-KEY-HERE"
 			},
-			// Our Assembly instructions just contain two Steps for now.
-			// You can name the Steps how you like.
-			steps: {
-				// The first Step resizes the uploaded image(s) to 125x125 pixels.
-				// The /image/resize robots ignores any files that are not images
-				// automatically.
-				resize_to_125: {
-					robot: "/image/resize",
-					use: ":original",
-					width: 125,
-					height: 125
-				},
-				// The second Step resizes the results further to 75x75 pixels.
-				// Notice how we "use" the output files of the "resize_to_125"
-				// step as our input here. We could use all kinds of Steps with
-				// various robots that "use" each other, making this perfect for
-				// any workflow.
-				resize_to_75: {
-					robot: "/image/resize",
-					use: "resize_to_125",
-					width: 75,
-					height: 75,
-					// We also add a sepia effect here just for fun.
-					// The /image/resize robot has a ton of available parameters.
-					sepia: 80
-				}
-			}
+			template_id: '275df800240e11e5a20c2dc0bd276748'
+		},
+		onSuccess: function(assembly) {
+			console.log(assembly);
+
+			var thisTeam = localStorage['$selectedteam'];
+			var thisUser = localStorage['firebase:session::mksequip'].uid;
+
+			var refUrl = 'https://mksequip.firebaseIO.com/';
+			console.log('selected team', window.selectedTeam);
+			console.log(refUrl);
+			var fb = new Firebase(refUrl);
+			console.log(window.selectedTeam);
+			console.log(assembly.assembly_id);
+			fb = fb.child('teams').child(window.selectedTeam).child('images').child(assembly.assembly_id);
+			fb.set(assembly.results);
+			window.location.href('/#/index.documents');
 		}
 	});
 });
