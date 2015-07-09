3313da33da34ee61729a66c504d52b53c6d0b58b
Christiane Kammerl <christiane@kammerl.com>
2015-07-09 15:35:09 -0700

    dev(specs) adds testing incentive aka flying unicorn

--- test/karma.config.js
+++ test/karma.config.js
@@ -31,7 +31,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'unicorn'],
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
