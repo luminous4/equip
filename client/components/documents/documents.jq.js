9804ca04118cfbf4a11a36e2d57af2d4caa26b43
Emily Shenfield <emily.shenfield@gmail.com>
2015-07-11 16:16:26 -0700

    refactor(docs): removed console.logs

--- client/components/documents/documents.jq.js
+++ client/components/documents/documents.jq.js
@@ -18,15 +18,10 @@ $(function() {
 			template_id: '275df800240e11e5a20c2dc0bd276748'
 		},
 		onSuccess: function(assembly) {
-			console.log(assembly);
 			var thisTeam = localStorage['$selectedteam'];
 			var thisUser = localStorage['firebase:session::mksequip'].uid;
 			var refUrl = 'https://mksequip.firebaseIO.com/';
-			console.log('selected team', window.selectedTeam);
-			console.log(refUrl);
 			var fb = new Firebase(refUrl);
-			console.log(window.selectedTeam);
-			console.log(assembly.assembly_id);
 			fb = fb.child('teams').child(window.selectedTeam).child('images').child(assembly.assembly_id);
 			fb.set(assembly.results);
 		}
--- client/components/home/home.ctrl.js
+++
@@ -1,5 +0,0 @@
-angular.module('equip')
-
-  .controller('HomeCtrl', function($rootScope, $location, $window, $scope, User, FirebaseFactory) {
-
-  });
